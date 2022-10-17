b496d12f48049970ccdd67f163e4138d24db2e78
0K9090 <78574005+0K9090@users.noreply.github.com>
2022-10-17 14:02:34 -0700

    it can build now

--- omm_builder.sh
+++ omm_builder.sh
@@ -1,7 +1,7 @@
-OMM_REPOSITORY="sm64ex-omm.sh"
+OMM_REPOSITORY="sm64ex-omm"
 OMM_REPOSITORY_URL="https://github.com/PeachyPeachSM64/${OMM_REPOSITORY}.git -b nightly"
 OMM_BUILDER_VERSION_URL="https://raw.githubusercontent.com/PeachyPeachSM64/${OMM_REPOSITORY}/master/omm_builder.py"
-OMM_VERSION_URL="https://raw.githubusercontent.com/0K9090/${OMM_REPOSITORY}/master/omm.mk"
+OMM_VERSION_URL="https://raw.githubusercontent.com/0K9090/sm64ex-omm.sh/master/omm.mk"
 OMM_META_PATCH_URL="https://raw.githubusercontent.com/PeachyPeachSM64/${OMM_REPOSITORY}/master/patch/omm.patch"
 OMM_PATCH_URL="https://raw.githubusercontent.com/PeachyPeachSM64/sm64ex-omm-resources/main/omm.patch.bin"
 DYNOS_PATCH_URL="https://raw.githubusercontent.com/PeachyPeachSM64/sm64ex-omm-resources/main/dynos.patch"
@@ -104,7 +104,7 @@ OMM_REPO_DESCRIPTIONS=("  PC Port of Super Mario 64 with additional features. Dy
 OMM_REPO_NAMES=("Super Mario 64 ex-nightly" "Super Mario 64 ex-alo" "Super Mario 64 Moonshine" "Super Mario 74" "Super Mario Star Road" "Super Mario 64: The Green Stars" "Render96")
 OMM_REPO_ABBR=("smex" "xalo" "smms" "sm74" "smsr" "smgs" "r96x")
 OMM_BUILD_DESCRIPTIONS_ERR=("  Compile the game with some customization." "PBE2" "PBE" "PBE" "PBE")
-OMM_BUILD_DESCRIPTIONS=("  Compile the game with some customization." "  Launch the compiled game." "  Clear the build/us_pc directory." "  Reset the game directory to its initial state, i.e. without patches applied." "Delete the game directory and all its content.")
+OMM_BUILD_DESCRIPTIONS=("  Compile the game with some customization." "  Launch the compiled game." "  Clear the build/us_pc directory." "  Reset the game directory to its initial state, i.e. without patches applied." "  Delete the game directory and all its content.")
 OMM_DEPENDS=("make" "git" "zip" "unzip" "7z" "cc" "gcc" "g++" "cpp")
 OMM_BUILD_SPEEDS=("Slow${BGC_LCYAN}                                                                                     " "Fast${BGC_LCYAN}                                                                                     " "Faster${BGC_LCYAN}                                                                                   " "Fastest${BGC_LCYAN}                                                                                  ")
 OMM_BUILD_SPEEDS_U=("Slow                                                                                     " "Fast                                                                                     " "Faster                                                                                   " "Fastest                                                                                  ")
@@ -113,9 +113,13 @@ OMM_RENDER_API_UNS=("OpenGL 2.1                                                 
 OMM_DYNOS_TOGGLE_S=("${BGC_LRED}Disabled${BGC_LCYAN}                                                                                 " "${BGC_LGREEN}Enabled${BGC_LCYAN}                                                                                  ")
 OMM_DYNOS_TOGGLE_UNS=("${COL_RED}Disabled                                                                                 " "${COL_GREEN}Enabled                                                                                  ")
 MENU_GAME_NAMES=("     Super Mario 64 ex-nightly        " "     Super Mario 64 ex-alo            " "     Super Mario 64 Moonshine         " "     Super Mario 74                   " "     Super Mario Star Road            " "     Super Mario 64: The Green Stars  " "     Render96                         ")
+GAME_REPOS=("https://github.com/sm64pc/sm64ex.git -b nightly" "https://github.com/AloXado320/sm64ex-alo.git -b master" "https://github.com/sm64pc/sm64ex.git -b nightly" "https://github.com/PeachyPeachSM64/sm64ex-omm.git -b sm74" "https://github.com/PeachyPeachSM64/sm64ex-omm.git -b smsr" "https://github.com/PeachyPeachSM64/sm64ex-omm.git -b smgs" "https://github.com/Render96/Render96ex.git -b tester")
+GAME_COMMITS=("" "b9283d080d8f82befe3917a916843cbfb1399411" "" "" "" "" "")
 MENU_SPEEDS=("   Slow                             " "   Fast                             " "   Faster                           " "   Fastest                          ")
 MENU_API=("    OpenGL 2.1                       " "    DirectX 11                       ")
 OMM_OPTIONS_DESCRIPTIONS=("  Building process duration. The faster, the more power-consuming." "  Backend used to render the game." "  Patch the latest version of DynOS to enable Model Packs support as well as an enhanced options menu." "  Doesn't work yet." "  Doesn't work yet." "  Currently unavailable." "  Currently unavailable." "  Currently unavailable." "  Compile the game.")
+OMM_MAKE_SPEEDS=("" " -j$(expr $(nproc) / 2)" " -j$(nproc)" " -j")
+OMM_MAKE_RAPI=("RENDER_API=GL WINDOW_API=SDL2 AUDIO_API=SDL2 CONTROLLER_API=SDL2 GRUCODE=f3dex2e" "RENDER_API=D3D11 WINDOW_API=DXGI AUDIO_API=SDL2 CONTROLLER_API=SDL2 GRUCODE=f3dex2e")
 dependcheck() {
 	miss=0
 	dcc=$(echo "python" | wc -m)
@@ -192,7 +196,213 @@ getinput() {
 	elif [ $1 == x ] || [ $1 == X ]; then
 		((screenid--))
 	elif [ $1 == c ] || [ $1 == C ]; then
-		((screenid++))
+		if [ $screenid == 3 ]; then
+			if [ $selected == 9 ]; then
+				screenid=make
+			elif [ $selected == 1 ]; then
+				if [ $lra -lt 4 ]; then
+					((lra++))
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				else
+					lra=1
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				fi
+			elif [ $selected == 2 ]; then
+				if [ $lrb -lt 2 ]; then
+					((lrb++))
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				else
+					lrb=1
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				fi
+			elif [ $screenid == 3 ]; then
+				if [ $lrc -lt 2 ]; then
+					((lrc++))
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				else
+					lrc=1
+					echologo
+					echo
+					echo -e "\033[A\r${COL_LCYAN}${FMT_BOLD} +----------------------------------------------------${FMT_RESET}${COL_LCYAN} Build (${COL_DEFAULT}${FMT_BOLD}9${FMT_RESET}${COL_LCYAN}) ${FMT_BOLD}-----------------------------------------------------+${FMT_RESET}"
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+					echo
+				fi
+			else
+				if [ $cant == 0 ] || [ h$cant == h ]; then
+					((screenid++))
+				fi
+			fi
+		elif [ $screenid == 2 ]; then
+			if ! [ $rerr == 1 ]; then
+				if [ $selected == 2 ]; then
+					cd repos/$ABBR/build/us_pc
+					clear
+					echo "--- Starting ${NAME}..."
+					./sm64.us.f3dex2e.exe
+					echo -e "\e[?25h"
+					stty echo
+					exit # when done, exit
+				else
+					if [ $cant == 0 ] || [ h$cant == h ]; then
+						if [ $screenid == 2 ]; then
+							if [ $selected == 5 ]; then
+								clear
+								echo "--- Deleting ${NAME}..."
+								rm -rf repos/${ABBR}
+								echo "Done."
+								echo -e "\e[?25h"
+								stty echo
+								exit
+							else
+								((screenid++))
+							fi
+						else
+							((screenid++))
+						fi
+					fi
+				fi
+			else
+				if [ $cant == 0 ] || [ h$cant == h ]; then
+					if [ $screenid == 2 ]; then
+						if [ $selected == 5 ]; then
+							clear
+							echo "--- Deleting ${NAME}..."
+							rm -rf repos/${ABBR}
+							echo "Done."
+							echo -e "\e[?25h"
+							stty echo
+							exit
+						else
+							((screenid++))
+						fi
+					else
+						((screenid++))
+					fi
+				fi
+			fi
+		else
+			if [ $cant == 0 ] || [ h$cant == h ]; then
+				if [ $screenid == 2 ]; then
+					if [ $selected == 5 ]; then
+						clear
+						echo "--- Deleting ${NAME}..."
+						rm -rf repos/${ABBR}
+						echo "Done."
+						echo -e "\e[?25h"
+						stty echo
+						exit
+					else
+						((screenid++))
+					fi
+				else
+					((screenid++))
+				fi
+			fi
+		fi
 	elif [ $screenid == 0 ]; then
 		echo -e "\e[?25h"
 		stty echo
@@ -438,6 +648,7 @@ getcustom() {
 	cd ../
 }
 
+echo -e "\e[?25l\033[A" # Hide cursor
 dependcheck
 OMM_PATCH_VERSION="7.3.2"
 OMM_SH_LOCAL_VERSION="1.0.1" # Local version
@@ -466,6 +677,24 @@ if ! [ "h$1" == "h--no-version-check" ]; then
 		fi
 		OMM_SH_VERSION+=$char
 	done
+	line=$(sed '36!d' omm.version)
+	b=0
+	while :; do
+		((b++))
+		char=${line:$b:1}
+		if [ "$char" == "=" ]; then
+			break
+		fi
+	done
+	((b++))
+	while :; do
+		((b++))
+		char=${line:$b:1}
+		if [ "h$char" == "h" ]; then
+			break
+		fi
+		OMM_PATCH_REVISION+=$char
+	done
 	if ! [ "$OMM_SH_LOCAL_VERSION" == "$OMM_SH_VERSION" ]; then
 		echo "Your OMM builder is not up-to-date."
 		read -sn 1 -p "Do you want to download and install the latest version? [y/n] " inp
@@ -476,14 +705,20 @@ if ! [ "h$1" == "h--no-version-check" ]; then
 			git pull -q
 			rm -rf custom
 			echo "Done."
+			OMM_SH_LOCAL_VERSION=$OMM_SH_VERSION
 		fi
 	fi
+	OMM_PATCH_DIRNAME="omm.${OMM_PATCH_VERSION}.${OMM_PATCH_REVISION}"
+	echo "Version: ${OMM_PATCH_VERSION}"
+	echo ".sh Version: ${OMM_SH_LOCAL_VERSION}"
+	echo "Revision: ${OMM_PATCH_REVISION}"
+	echo "Directory: ${OMM_PATCH_DIRNAME}"
+	rm omm.version
 fi
 echo "--- Initializing..."
 getcustom
 stty -echo            # Makes it so when you type it doesn't display the characters
 printf '\e[8;27;120t' # resize to 27 rows and 120 cols
-echo -e "\e[?25l"     # Hide cursor
 clear
 screenid=1
 echologo() {
@@ -508,6 +743,7 @@ echologo() {
 	echo
 }
 menu() {
+	cant=0
 	if [ $screenid == 0 ]; then
 		clear
 		echo -e "\e[?25h" # Show cursor
@@ -583,6 +819,8 @@ menu() {
 			getinput $MENU_INPUT
 			ABBR="${OMM_REPO_ABBR[$(expr $selected - 1)]}"
 			NAME="${OMM_REPO_NAMES[$(expr $selected - 1)]}"
+			REPO="${GAME_REPOS[$(expr $selected - 1)]}"
+			COMMIT="${GAME_COMMITS[$(expr $selected - 1)]}"
 			NAMEN=$selected
 			if ! [ $screenid == 1 ]; then
 				break
@@ -614,17 +852,18 @@ menu() {
 			cd repos
 			if [ -d $ABBR ]; then
 				err=0
-				cd $ABBR
-				if [ -f build/us_pc/sm64.us.f3dexe2.exe ]; then
+				if [ -f $ABBR/build/us_pc/sm64.us.f3dex2e.exe ]; then
 					rerr=0
 				fi
 			else
 				err=1
 			fi
+			cd ../
 		else
 			err=1
 		fi
 		while :; do
+			cant=0
 			if [ -f baserom.us.z64 ]; then
 				if [ $selected == 1 ]; then
 					echo -e "\033[16A\r ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${BGC_LCYAN}${COL_BLACK}<1>  Build                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
@@ -633,6 +872,7 @@ menu() {
 				fi
 			else
 				if [ $selected == 1 ]; then
+					cant=1
 					echo -e "\033[16A\r ${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}${BGC_GRAY}${COL_BLACK}<1>  Build                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				else
 					echo -e "\033[16A\r ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${COL_GRAY}<1>  Build                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
@@ -640,6 +880,7 @@ menu() {
 			fi
 			if [ $rerr == 1 ]; then
 				if [ $selected == 2 ]; then
+					cant=1
 					echo -e " ${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}${BGC_GRAY}${COL_BLACK}<2>  Run                                                                                                           ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				else
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${COL_GRAY}<2>  Run                                                                                                           ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
@@ -678,16 +919,19 @@ menu() {
 				DESC="${OMM_BUILD_DESCRIPTIONS[$(expr $selected - 1)]}"
 			else
 				if [ $selected == 3 ]; then
+					cant=1
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${BGC_GRAY}${COL_BLACK}<3>  Clear                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				else
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${COL_GRAY}<3>  Clear                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				fi
 				if [ $selected == 4 ]; then
+					cant=1
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${BGC_GRAY}${COL_BLACK}<4>  Reset                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				else
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${COL_GRAY}<4>  Reset                                                                                                         ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				fi
 				if [ $selected == 5 ]; then
+					cant=1
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${BGC_GRAY}${COL_BLACK}<5>  Delete                                                                                                        ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
 				else
 					echo -e " ${COL_LCYAN}${FMT_BOLD}|${FMT_RESET} ${COL_GRAY}<5>  Delete                                                                                                        ${FMT_RESET}${COL_LCYAN}${FMT_BOLD}| ${FMT_RESET}"
@@ -826,6 +1070,85 @@ menu() {
 				break
 			fi
 		done
+	elif [ "$screenid" == "make" ]; then
+		clear
+		echo "--- Getting OMM repo..."
+		if [ -d $OMM_PATCH_DIRNAME ]; then
+			rm -rf $OMM_PATCH_DIRNAME
+		fi
+		git clone --single-branch ${OMM_REPOSITORY_URL} ${OMM_PATCH_DIRNAME}
+		if ! [ -d $OMM_PATCH_DIRNAME ]; then
+			echo -e "${COL_RED}Cannot clone the git repository: ${OMM_REPOSITORY_URL}${COL_DEFAULT}"
+		fi
+		freshclone=0
+		if ! [ -d repos/${ABBR} ]; then
+			if ! [ -d repos ]; then
+				mkdir repos
+			fi
+			echo "--- Building ${NAME}..."
+			echo "--- Cloning ${NAME} repository..."
+			git clone --single-branch ${REPO} repos/${ABBR}
+			if ! [ -d repos/${ABBR} ]; then
+				echo -e "${COL_RED}Cannot clone the git repository: ${ABBR}${COL_DEFAULT}"
+				echo -e "\e[?25h"
+				stty echo
+				exit
+			fi
+			freshclone=1
+		fi
+		cd repos/${ABBR}
+		git reset -q --hard ${COMMIT}
+		echo "--- Eliminating bad files..."
+		echo >>filelist "$(ls -1)"
+		i=0
+		while :; do
+			((i++))
+			if [ $i -gt $(sed -n '$=' filelist) ]; then
+				break
+			fi
+			cline=$(sed $i'!d' filelist)
+			if [[ "$cline" == *".png.png"* ]]; then
+				rm $cline
+			fi
+		done
+		rm filelist
+		DYNOSARG=
+		if [ $lrc == 2 ]; then
+			DYNOSARG=" dynos "
+			echo "--- Applying DynOS patch..."
+			wget --no-check-certificate --no-cache --no-cookies ${DYNOS_PATCH_URL} -O dynos.patch -q
+			git apply --reject --whitespace=nowarn "dynos.patch"
+			rm dynos.patch
+		fi
+		echo "--- Applying OMM patch..."
+		cp -rf ../../${OMM_PATCH_DIRNAME}/. .
+		if ! [ -f Makefile ]; then
+			echo -e "${COL_RED}Missing Makefile.${COL_DEFAULT}"
+			echo -e "\e[?25h"
+			stty echo
+			exit
+		fi
+		echo "python3 omm_patcher.py -p ${ABBR}${DYNOSARG}"
+		python3 omm_patcher.py -p ${ABBR}${DYNOSARG}
+		echo "--- Building game..."
+		cp -f ../../baserom.us.z64 baserom.us.z64
+		echo "make${OMM_MAKE_SPEEDS[$(expr $lra - 1)]} OMM_BUILDER=1 VERSION=us ${OMM_MAKE_RAPI[$(expr $lrb - 1)]}"
+		make${OMM_MAKE_SPEEDS[$(expr $lra - 1)]} OMM_BUILDER=1 VERSION=us ${OMM_MAKE_RAPI[$(expr $lrb - 1)]}
+		echo "--- Starting ${NAME}..."
+		chmod 755 -f -R ./build/us_pc/res
+		chmod 755 -f -R ./build/us_pc/dynos
+		cd build/us_pc
+		if ! [ -f sm64.us.f3dex2e.exe]; then
+			echo -e "${COL_RED}Game executable not found.${COL_DEFAULT}"
+			echo -e "\e[?25h"
+			stty echo
+			exit
+		fi
+		chmod 755 sm64.us.f3dex2e.exe
+		./sm64.us.f3dex2e.exe
+		echo -e "\e[?25h"
+		stty echo
+		exit # when done, exit
 	else
 		clear
 		echo -e "\e[?25h"
